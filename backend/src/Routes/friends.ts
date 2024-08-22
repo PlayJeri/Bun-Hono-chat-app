7bf1e5f0b69f50858a490947c494cc2a32a981c5
PlayJeri <nurmi.jeri@gmail.com>
2024-08-23 02:18:40 +0300

    add endpoint to get contacts online

--- backend/src/Routes/friends.ts
+++ backend/src/Routes/friends.ts
@@ -4,39 +4,66 @@ import { ContextVariables } from "../Types";
 import { db } from "../db/database";
 import { contact, user } from "../db/schema";
 import { eq } from "drizzle-orm";
+import { currentUsersMap } from "../Ws/currentUsers";
 
 const friends = new Hono<{ Variables: ContextVariables }>();
 
 friends.use(checkAccessToken);
 
-friends.get("/", async (c) => {
-  const userId = c.get("decodedPayload").id;
-  return c.json({ message: "Hello Friends" });
+friends.get("/online", async (c) => {
+	try {
+		const userId = c.get("decodedPayload").id;
+		const onlineContacts: { username: string; id: number }[] = [];
+
+		const dbContacts = await db
+			.select({ friendId: contact.friendId })
+			.from(contact)
+			.where(eq(contact.userId, userId));
+
+		dbContacts.forEach((contact) => {
+			const user = currentUsersMap.get(contact.friendId);
+			console.log(currentUsersMap.keys());
+			console.log(user);
+			if (user != undefined) {
+				onlineContacts.push({ username: user.username, id: user.id });
+			}
+		});
+
+		return c.json(onlineContacts);
+	} catch (error) {
+		console.error("Get online friends error: ", error);
+		return c.json({ message: "Internal server error" }, 500);
+	}
 });
 
 friends.post("/add", async (c) => {
-  try {
-    const { friendId } = await c.req.json();
-    const userId = c.get("decodedPayload").id;
+	try {
+		const { friendId } = await c.req.json();
+		const userId = c.get("decodedPayload").id;
 
-    if (userId == friendId) {
-      return c.json({ message: "Invalid request" }, 400);
-    }
+		if (userId == friendId) {
+			return c.json({ message: "Invalid request" }, 400);
+		}
 
-    const users = await db.select().from(user).where(eq(user.id, friendId));
+		const users = await db.select().from(user).where(eq(user.id, friendId));
 
-    if (users.length === 0) {
-      return c.json({ message: "User not found" }, 400);
-    }
+		if (users.length === 0) {
+			return c.json({ message: "User not found" }, 400);
+		}
 
-    const dbUser = users[0];
+		const dbUser = users[0];
 
-    db.insert(contact).values({ userId, friendId });
+		const res = await db
+			.insert(contact)
+			.values({ userId, friendId })
+			.returning();
+		console.log(res);
+		console.log(userId, friendId);
 
-    return c.json({ message: "Added" });
-  } catch (error) {
-    return c.json({ message: "Internal Server Error" }, 500);
-  }
+		return c.json({ message: "Added" });
+	} catch (error) {
+		return c.json({ message: "Internal Server Error" }, 500);
+	}
 });
 
 export { friends };
